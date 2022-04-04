22fac5703bcc4c555d3d9dfc1fd59bb0a21a0761
MattRuetz <matt.b.ruetz@gmail.com>
2022-04-04 15:42:58 -0400

    setting up child pages. TODO: fix list child pages on parent page-links div

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -13,3 +13,11 @@ function university_files()
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features()
+{
+    // browser tab shows current page title
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'university_features');
