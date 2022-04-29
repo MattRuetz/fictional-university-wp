9fdd3e7dbda72a9f8e9f00aef0bcc8338e0a97d3
MattRuetz <matt.b.ruetz@gmail.com>
2022-04-29 19:57:50 -0400

    sanitized all inputs, note posts made private, max 5 notes per user set in functions.php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -200,6 +200,15 @@ add_filter('wp_insert_post_data', 'makeNotePrivate');
 
 function makeNotePrivate($data)
 {
+
+    if ($data['post_type'] == 'note') {
+        if (count_user_posts(get_current_user_id(), 'note') >= 5) {
+            die('You have reached your post limit!');
+        }
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    }
+
     if ($data['post_type'] == 'note' and $data['post_status'] != 'trash') {
         $data['post_status'] = 'private';
     }
